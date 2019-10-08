27ecfac8782d29e3fdddfaf1d1e157328dfec1bc
ramitgangwar <43425662+ramitgangwar@users.noreply.github.com>
2019-10-08 23:03:23 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -5,7 +5,7 @@ class Translation(object):
     Support Group :- @InFoTelGroup and @CuratorSCrew """
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "No Need To Pay Money Just Use Free Functions.If You Wanna Donate Then Just Message <a href="https://t.me/TheDarkW3b"Me </a> 🙂"
+    UPGRADE_TEXT = "No Need To Pay Money Just Use Free Functions.If You Wanna Donate Then Just Message <a href='https://t.me/TheDarkW3b'Me</a> 🙂"
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
